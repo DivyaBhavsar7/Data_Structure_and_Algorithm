e1f5a95a6cdc508c74139f02a926099a66cd4423
Divya Bhavsar <96143831+DivyaBhavsar7@users.noreply.github.com>
2023-09-21 21:10:34 +0530

    Create Postorder.java

---
+++ Tree/Postorder.java
@@ -0,0 +1,71 @@
+package practise_tree_graph;
+import java.util.Scanner;
+class Node
+{
+	int data;
+	Node left,right;
+	Node(int e)
+	{
+		data=e;
+		right=left=null;
+	}
+}
+public class Tree {
+	
+	private Node root;
+	static int c=0,c1=0;
+	Tree()
+	{
+		root=null;
+	}
+	void tree(Node r,Node n)
+	{
+		if(root==null)
+			root=n;
+		else
+		{
+			if(r.data>n.data)
+				if(r.left==null)
+					r.left=n;
+				else
+					tree(r.left, n);
+			else
+			{
+				if(r.right==null)
+					r.right=n;
+				else
+					tree(r.right,n);
+			}
+		}
+	}
+	void postorder(Node r) {
+		if(r!=null)
+		{
+		    postorder(r.left);
+			postorder(r.right);
+			System.out.print(r.data+"  ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		Tree t=new Tree();
+		
+		Node n1=new Node(86);
+		Node n2=new Node(56);
+		Node n3=new Node(78);
+		Node n4=new Node(103);
+		Node n5=new Node(42);
+		
+		t.tree(t.root, n1);
+		t.tree(t.root, n2);
+		t.tree(t.root, n3);
+		t.tree(t.root, n4);
+		t.tree(t.root, n5);
+
+	    System.out.println("\nPostorder");
+	    t.postorder(t.root);
+
+	}
+
+}
